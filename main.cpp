9858f8505e87d11adb6085a968deba8f914fd731
Eugenia Guerrero <angeleuge95@gmail.com>
2017-11-12 16:14:41 +0000

    added WIP

--- main.cpp
+++ main.cpp
@@ -14,22 +14,36 @@ int main(int argc, char** argv){
 
   for (int i = 1; i < argc; i++){
     string config_filename = argv[i];
+    string ext = get_extension(config_filename);
     if (!can_open_file(config_filename)){
       cout << "Configuration file cannot be opened" << endl;
       return ERROR_OPENING_CONFIGURATION_FILE;
-    } else if (!contains_only_nums(config_filename)) {
-      cout << "Configuration file does not contain only numbers" << endl;
-      return NON_NUMERIC_CHARACTER;
-    } else if (!contains_valid_indexes(config_filename)){
-      cout << "Configuration file contains an invalid index" << endl;
-      return INVALID_INDEX;
-    }
+    } if (!contains_only_nums(config_filename)) {
+        cout << "Configuration file does not contain only numbers" << endl;
+        return NON_NUMERIC_CHARACTER;
+      } else if (!contains_valid_indexes(config_filename)){
+        cout << "Configuration file contains an invalid index" << endl;
+        return INVALID_INDEX;
+      } else if (ext == "pb" && !valid_plugboard_configuration(config_filename)) {
+        cout << "Impossible plugboard configuration" << endl;
+        return IMPOSSIBLE_PLUGBOARD_CONFIGURATION;
+      } else if (ext == "pb" && !valid_plugboard_parameters(config_filename)) {
+        cout << "Incorrect number of plugboard parameters" << endl;
+        return INCORRECT_NUMBER_OF_PLUGBOARD_PARAMETERS;
+      } else if (ext == "rot" && !valid_rotor_mapping(config_filename)){
+        cout << "Invalid rotor mapping" << endl;
+        return INVALID_ROTOR_MAPPING;
+      } else if (ext == "rot" && !valid_rotor_start_position(config_filename)){
+        cout << "No rotor starting position" << endl;
+        return NO_ROTOR_STARTING_POSITION;
+      } else if (ext == "rf" && !valid_reflector_mapping(config_filename)){
+        cout << "Invalid reflector mapping" << endl;
+        return INVALID_REFLECTOR_MAPPING;
+      } else if (ext == "rf" && !valid_reflector_parameters(config_filename)){
+        cout << "Incorrect number of reflector parameters" << endl;
+        return INCORRECT_NUMBER_OF_REFLECTOR_PARAMETERS;
+      }
   }
-  // specific checks for configuration files
-  // if (!valid_plugboard_config(config_filename)){
-  //
-  // }
-
 
   return NO_ERROR;
 }
@@ -44,7 +58,6 @@ int* get_configuration(string config_filename){
     numbers[n] = stoi(token);
     n++;
   }
-
   in_stream.close();
   return numbers;
 }
@@ -58,7 +71,6 @@ bool can_open_file(string config_filename){
     return false;
   }
   in_stream.close();
-
   return true;
 }
 
@@ -95,7 +107,39 @@ bool contains_valid_indexes(string config_filename){
       return false;
     }
   }
-
   in_stream.close();
+  return true;
+}
+
+string get_extension(string config_filename){
+  int i_dot = rfind(config_filename, ".");
+  if (i_dot == npos) {
+    return "";
+  }
+  return config_filename.substr(i_dot + 1, config_filename.length());
+}
+
+
+bool valid_plugboard_configuration(string config_filename){
+  return true;
+}
+
+bool valid_plugboard_parameters(string config_filename){
+  return true;
+}
+
+bool valid_rotor_mapping(string config_filename){
+  return true;
+}
+
+bool valid_rotor_start_position(string config_filename){
+  return true;
+}
+
+bool valid_reflector_mapping(string config_filename){
+  return true;
+}
+
+bool valid_reflector_parameters(string config_filename){
   return true;
 }
--- main.h
+++ main.h
@@ -5,3 +5,9 @@ bool can_open_file(string config_filename);
 bool is_num(string token);
 bool contains_only_nums(string config_filename);
 bool contains_valid_indexes(string config_filename);
+// specific checks for configuration files
+bool valid_config_file(string config_filename);
+bool valid_pb_file(string config_filename);
+bool valid_rot_file(string config_filename);
+bool valid_rf_file(string config_filename);
+string get_extension(string config_filename);
