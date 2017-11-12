b2a585f9486c3ce9d1f052cdcd368a8c06643610
Eugenia Guerrero <angeleuge95@gmail.com>
2017-11-12 19:26:22 +0000

    added checks for asserting whether elem was contained in array

--- main.cpp
+++ main.cpp
@@ -15,62 +15,54 @@ int main(int argc, char** argv){
   for (int i = 1; i < argc; i++){
     string config_filename = argv[i];
     string ext = get_extension(config_filename);
-    if (!can_open_file(config_filename)){
-      cout << "Configuration file cannot be opened" << endl;
-      return ERROR_OPENING_CONFIGURATION_FILE;
-    } if (!contains_only_nums(config_filename)) {
+    string token;
+    int j = 0;
+    int numbers[26];
+
+    ifstream in_stream(config_filename.c_str());
+    while(getline(in_stream, token, ' ')) {
+      if (!can_open_file(in_stream)){
+        cout << "Configuration file cannot be opened" << endl;
+        return ERROR_OPENING_CONFIGURATION_FILE;
+      } if (!is_num(token)) {
         cout << "Configuration file does not contain only numbers" << endl;
         return NON_NUMERIC_CHARACTER;
-      } else if (!contains_valid_indexes(config_filename)){
+      } else if (!valid_index(token)){
         cout << "Configuration file contains an invalid index" << endl;
         return INVALID_INDEX;
-      } else if (ext == "pb" && !valid_plugboard_configuration(config_filename)) {
+      } else if (ext == "pb" && !contains(stoi(token), numbers, j)) {
         cout << "Impossible plugboard configuration" << endl;
         return IMPOSSIBLE_PLUGBOARD_CONFIGURATION;
-      } else if (ext == "pb" && !valid_plugboard_parameters(config_filename)) {
+      } else if (ext == "pb" && !valid_plugboard_parameters(token)) {
         cout << "Incorrect number of plugboard parameters" << endl;
         return INCORRECT_NUMBER_OF_PLUGBOARD_PARAMETERS;
-      } else if (ext == "rot" && !valid_rotor_mapping(config_filename)){
+      } else if (ext == "rot" && !valid_rotor_mapping(token)){
         cout << "Invalid rotor mapping" << endl;
         return INVALID_ROTOR_MAPPING;
-      } else if (ext == "rot" && !valid_rotor_start_position(config_filename)){
+      } else if (ext == "rot" && !valid_rotor_start_position(token)){
         cout << "No rotor starting position" << endl;
         return NO_ROTOR_STARTING_POSITION;
-      } else if (ext == "rf" && !valid_reflector_mapping(config_filename)){
+      } else if (ext == "rf" && !contains(stoi(token), numbers, j)){
         cout << "Invalid reflector mapping" << endl;
         return INVALID_REFLECTOR_MAPPING;
-      } else if (ext == "rf" && !valid_reflector_parameters(config_filename)){
+      } else if (ext == "rf" && !valid_reflector_parameters(token)){
         cout << "Incorrect number of reflector parameters" << endl;
         return INCORRECT_NUMBER_OF_REFLECTOR_PARAMETERS;
       }
+      numbers[j] = stoi(token);
+      j++;
+    }
+    in_stream.close();
   }
-
   return NO_ERROR;
 }
 
-int* get_configuration(string config_filename){
-  string token;
-  int numbers[52];
-  ifstream in_stream(config_filename.c_str());
-
-  int n = 0;
-  while(getline(in_stream, token, ' ')) {
-    numbers[n] = stoi(token);
-    n++;
-  }
-  in_stream.close();
-  return numbers;
-}
-
-bool can_open_file(string config_filename){
-  ifstream in_stream(config_filename.c_str());
-
+bool can_open_file(ifstream &in_stream){
   if (in_stream.fail())
   {
     cout << "Configuration file cannot be opened." << endl;
     return false;
   }
-  in_stream.close();
   return true;
 }
 
@@ -85,61 +77,43 @@ bool is_num(string token){
   return true;
 }
 
-bool contains_only_nums(string config_filename){
-  string token;
-  ifstream in_stream(config_filename.c_str());
-
-  while(getline(in_stream, token, ' ')) {
-    if (!is_num(token)){
-      return false;
-    }
-  }
-  in_stream.close();
-  return true;
-}
-
-bool contains_valid_indexes(string config_filename){
-  string token;
-  ifstream in_stream(config_filename.c_str());
-
-  while(getline(in_stream, token, ' ')) {
-    if (!((stoi(token) >= 0) && (stoi(token) <= 25))){
-      return false;
-    }
-  }
-  in_stream.close();
-  return true;
+bool valid_index(string token){
+  return (stoi(token) >= 0) && (stoi(token) <= 25);
 }
 
 string get_extension(string config_filename){
-  int i_dot = rfind(config_filename, ".");
-  if (i_dot == npos) {
+  // find last dot in given string
+  int ext_index = config_filename.rfind(".");
+  // no extension found, return empty string
+  if (ext_index == string::npos) {
     return "";
   }
-  return config_filename.substr(i_dot + 1, config_filename.length());
+  // otherwise return substring after the last dot
+  return config_filename.substr(ext_index + 1, config_filename.length());
 }
 
-
-bool valid_plugboard_configuration(string config_filename){
+// size of numbers array needs to be explicit to get start and end of array
+bool contains(int elem, int (&array)[26], int array_size){
+  for (int i = 0; i < array_size; i++) {
+    if (array[i] == elem) {
+      return false;
+    }
+  }
   return true;
 }
 
-bool valid_plugboard_parameters(string config_filename){
+bool valid_plugboard_parameters(string token){
   return true;
 }
 
-bool valid_rotor_mapping(string config_filename){
+bool valid_rotor_mapping(string token){
   return true;
 }
 
-bool valid_rotor_start_position(string config_filename){
+bool valid_rotor_start_position(string token){
   return true;
 }
 
-bool valid_reflector_mapping(string config_filename){
-  return true;
-}
-
-bool valid_reflector_parameters(string config_filename){
+bool valid_reflector_parameters(string token){
   return true;
 }
--- main.h
+++ main.h
@@ -1,13 +1,16 @@
 using namespace std;
 
 int* get_configuration(string config_filename);
-bool can_open_file(string config_filename);
+bool can_open_file(ifstream &in_stream);
 bool is_num(string token);
-bool contains_only_nums(string config_filename);
-bool contains_valid_indexes(string config_filename);
+bool valid_index(string token);
 // specific checks for configuration files
-bool valid_config_file(string config_filename);
-bool valid_pb_file(string config_filename);
-bool valid_rot_file(string config_filename);
-bool valid_rf_file(string config_filename);
-string get_extension(string config_filename);
+bool valid_pb_file(string token);
+bool valid_rot_file(string token);
+bool valid_rf_file(string token);
+string get_extension(string token);
+bool contains(int elem, int (&array)[26], int array_size);
+bool valid_plugboard_parameters(string token);
+bool valid_rotor_mapping(string token);
+bool valid_rotor_start_position(string token);
+bool valid_reflector_parameters(string token);
